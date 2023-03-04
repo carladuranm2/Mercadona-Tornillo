e6f32325b5d958a4e606099ebb7c15edb1fae5af
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2023-03-04 16:52:57 +0100

    feat: modify module on project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoAccessGuard } from './guards/no-access.guard';
+import {LoginModule} from './components/auth/login/login.module'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',loadChildren:()=>import('./components/home/home.module').then(m=> m.HomeModule),data:{Animation:'left'}},
+  { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule), data: {animation: 'fadeIn'}, canActivate:[NoAccessGuard] },
+  {path:'tornillo',loadChildren:()=>import('./components/tornillo/tornillo.module').then(m=> m.TornilloModule),data:{Animation:'left'}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    SharedModule,
+    MatSliderModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
