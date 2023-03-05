4b57bfd791cf2ed584d44a1c98a4106178e0f089
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2023-03-05 13:03:10 +0100

    feat: add no-acces guard

--- src/app/guards/no-access.guard.ts
+++ src/app/guards/no-access.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoAccessGuard implements CanActivate {
-  canActivate(
+  constructor(private router: Router) { }
+  canActivate (
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    let token = localStorage.getItem('token');
+    if (token) {
+      console.log("entre al if");
+      return true;
+        }else{
+          this.router.navigate(['/login']);
+          return false;
+        }
+    }
   }
-  
-}
