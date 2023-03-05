723446a4c566f5006278162115f7d9df84cf4639
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2023-03-05 13:01:23 +0100

    feat: change viewencapsulation on compoenent login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,24 +1,39 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { loginUser } from 'src/app/models/loginUser';
+import Swal from 'sweetalert2';
+
 
 @Component({
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.Emulated,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
 
   }
   loginUser: loginUser = {
-    email: "",
-    password: ""
+    email: "carladuranm2@gmail.com",
+    password: "carlaDuran"
   }
 
-  submit(){
-    this.router.navigateByUrl('/tornillo');
+  submit () {
+    if (this.loginUser.email ===  "carladuranm2@gmail.com" && this.loginUser.password ==="carlaDuran") {
+      const token = "11111"
+      localStorage.setItem('token', token);
+      this.router.navigateByUrl('/tornillo');
+    }else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Las credenciales son incorrectas, intentalo de nuevo!',
+      })
+      this.router.navigateByUrl('/');
+
+    }
+
   }
 }
