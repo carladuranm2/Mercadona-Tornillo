47a0b2480c6126ba42120ee72e983e1ceca7d47f
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2023-03-05 13:03:39 +0100

    fix: function incrementar cantidad

--- src/app/components/tornillo/components/crear-tornillo/crear-tornillo.component.ts
+++ src/app/components/tornillo/components/crear-tornillo/crear-tornillo.component.ts
@@ -1,32 +1,40 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import Swal from 'sweetalert2';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-crear-tornillo',
   templateUrl: './crear-tornillo.component.html',
   styleUrls: ['./crear-tornillo.component.scss']
 })
 export class CrearTornilloComponent implements OnInit {
   newProduct: FormGroup;
-  public cantidad: number = 0
-  constructor(public dialogRef: MatDialogRef<CrearTornilloComponent>,@Inject(MAT_DIALOG_DATA,) public data: any, private fb: FormBuilder,){
+  public cantidad: number = 0;
+  constructor(public dialogRef: MatDialogRef<CrearTornilloComponent>, @Inject(MAT_DIALOG_DATA,) public data: any, private fb: FormBuilder,) {
 
   }
-  ngOnInit() {
+  ngOnInit () {
     this.newProduct = this.fb.group({
       nombre: new FormControl('', Validators.required),
-      precio: new FormControl(0, Validators.required),
-      formato: new FormControl('',Validators.required),
-      marca:  new FormControl('',Validators.required)
+      precio: new FormControl('', Validators.required),
+      formato: new FormControl('', Validators.required),
+      marca: new FormControl('', Validators.required)
     });
   }
 
-  onSubmit(){
-    if(this.newProduct.valid){
+  onSubmit () {
+    if (this.newProduct.valid) {
       this.data.name.push(this.newProduct.value);
-    }else {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: `Ha sido guardado exitosamente ${this.newProduct.value.nombre}`,
+        showConfirmButton: false,
+        timer: 2000
+      })
+    } else {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -35,18 +43,20 @@ export class CrearTornilloComponent implements OnInit {
     }
     this.dialogRef.close();
   }
-  incrementarCantidad(){
-    this.cantidad = this.cantidad + 1;
-    this.newProduct.value.precio.setValue(this.cantidad);
-  }
-  decrementarCantidad(){
 
-    if(this.cantidad===0){
+  incrementarCantidad () {
+    this.cantidad = this.cantidad + 1;
+    this.newProduct.get('precio').setValue(this.cantidad);
+    console.log(this.cantidad, "k e paza");
+
+  }
+  decrementarCantidad () {
+    if (this.cantidad === 0) {
       this.cantidad = this.cantidad - 0;
-      this.newProduct.value.precio.setValue(this.cantidad);
-    }else {
+      this.newProduct.get('precio').setValue(this.cantidad);
+    } else {
       this.cantidad = this.cantidad - 1;
-      this.newProduct.value.precio.setValue(this.cantidad);
+      this.newProduct.get('precio').setValue(this.cantidad);
     }
   }
 }
