38ee02fdcecce17b7c8abfc0088b292bab924f63
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2023-03-05 13:05:56 +0100

    feat: add loading progress

--- src/app/shared/loading-progress/loading-progress.component.ts
+++ src/app/shared/loading-progress/loading-progress.component.ts
@@ -1,22 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-loading-progress',
   templateUrl: './loading-progress.component.html',
   styleUrls: ['./loading-progress.component.scss']
 })
 export class LoadingProgressComponent {
-  @Input('total') goal:number = 0;
-  @Input('value') currentProgress:number = 0;
-  @Input('color') color:any="#51a73b";
-  public height:number = 8;
-  public colorTinyLine="#f3f3f5"
-  public heightTinyLine:number = 3;
+  @Input() isLoading = true;
 
-  getProgressValue(value:number){
-    if (value>100) {
-      return 100;
-    }
-    return value;
-  }
 }
