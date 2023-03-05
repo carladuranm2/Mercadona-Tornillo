b45568ae06cf4f604f6d3336ba6cfbfe316227e7
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2023-03-05 13:02:21 +0100

    feat: add route on getToLogin

--- src/app/components/home/components/home-page-tornillos/home-page-tornillos.component.ts
+++ src/app/components/home/components/home-page-tornillos/home-page-tornillos.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-home-page-tornillos',
   templateUrl: './home-page-tornillos.component.html',
   styleUrls: ['./home-page-tornillos.component.scss']
 })
 export class HomePageTornillosComponent {
+constructor(private router: Router){
 
+}
+  goToLogin(){
+    this.router.navigate(['/login']);
+  }
 }
